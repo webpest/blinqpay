305199e2174536e5775d16906d3921299083c749
Abayomi Oloyinde <abayomi.oloyinde@digimatix.tech>
2024-06-28 14:43:37 +0100

    intial push

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,12 +1,31 @@
 "use client";
 import Tabs from "./Tabs";
 import { useGetCategoriesQuery } from "@/lib/features/products/productsApiSlice";
+import { useState, useEffect } from "react";
 
 const Menu = ({ handleFilter }: { handleFilter: any }) => {
   const { data: categories } = useGetCategoriesQuery();
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsFixed(scrollPosition > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <div>
+    <div
+      className={`bg-white ${
+        isFixed ? "sticky top-[75px] border-b border-b-borderColor" : ""
+      }`}
+    >
       <div className="container mx-auto">
         <Tabs tabs={categories} handleFilter={handleFilter} />
       </div>
